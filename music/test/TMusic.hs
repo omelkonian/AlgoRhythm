e85d059a8ad117f6dec0e2379d68f16891164ba3
Orestis Melkonian <melkon.or@gmail.com>
2018-03-05 20:03:07 +0100

    Tests: added test case for transposing bigger piece

--- music/test/TMusic.hs
+++ music/test/TMusic.hs
@@ -13,9 +13,21 @@ import           GenSetup
 
 import Debug.Trace
 
-musicTests = testGroup "Music"
-  [ testCase "example" $
-      C<@4 <| (5%4) ~> M3 @?= E<@4 <| (5%4)
+musicTests = testGroup "Music" [transpTests]
+
+transpTests = testGroup "Tranpose"
+  [ testCase "one note" $
+      C<@4 <| 5%4 ~> M3 @?= E<@4 <| 5%4
+  , testCase "one chord" $
+      let a  = chord [p<@4<|4%4 | p <- [C, E, G]]
+          a' = chord [p<@4<|4%4 | p <- [Cs, F, Gs]]
+      in  a ~> Min2 @?= a'
+  , testCase "sequence of chords" $
+      let a  = chord [p<@4<|4%4 | p <- [C, E, G]]
+          a' = chord [p<@4<|4%4 | p <- [Cs, F, Gs]]
+          b  = chord [p<@4<|4%4 | p <- [D, Fs, A]]
+          b' = chord [p<@4<|4%4 | p <- [Ds, G, As]]
+      in  line [a, b, a] ~> Min2 @?= line [a', b', a']
   , testProperty "identityUp" $ \(p :: Pitch) (d :: Duration) ->
       (p<|d ~> P1) == (p<|d)
   , testProperty "identityUp" $ \(p :: Pitch) (d :: Duration) ->
