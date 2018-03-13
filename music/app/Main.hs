2224bc4362a35931774926532af58f8dc68f0264
Cas van der Rest <casmb11@xs4all.nl>
2018-03-13 23:22:07 +0100

    further work on the generator

--- music/app/Main.hs
+++ music/app/Main.hs
@@ -12,45 +12,6 @@ main = do
   x <- runGenerator () example
   writeToMidiFile "gen.midi" x
   writeToLilypondFile "gen.ly" x
-  where
-    cIonian, eBlues, scalePiece :: Melody
-    scalePiece = cIonian :+: Rest wn :+: (8 ## eBlues)
-    cIonian = line $ (C#4)+|major <||(qn^^^)
-    eBlues  = line $ (E#4)+|blues <||(qn^^^)
-
-    piece, piece' :: Music FullPitch
-    piece' = line $ map ($ piece) [id, (~> P4), id, (~> P5), (~> P4), id]
-    piece = line [ pc # oct <: [Dynamic dyn, Articulation art] <| dur
-                 | pc  <- [B, Fs, D, E]
-                 | oct <- [4, 4, 3, 3]
-                 | dur <- [(en^^^), (en^^^), (en^^^), (qn^.)]
-                 | dyn <- [PPP,FFF,PPP,FFF]
-                 | art <- [Staccatissimo,Tenuto,Marcato,Staccato]
-                 ]
-
-    -- generateSome :: MusicGenerator Melody
-    -- generateSome = do
-    --   chords <- replicateM 4 (genChord 5)
-    --   notes  <- replicateM 10 (genNote)
-    --   return ((line chords) :=: line notes)
-
-    bluesProgression :: Pitch -> Harmony
-    bluesProgression p =
-      let tonic = p=|d7 <| wn
-      in  line $ map ($ tonic) [ id     , id     , id, id
-                               , (~> P4), (~> P4), id, id
-                               , (~> P5), (~> P5), id, id
-                               ]
-    cBlues :: Harmony
-    cBlues = 4 ## bluesProgression (C#3)
-
-    cBluesImprov :: Melody
-    cBluesImprov = chords cBlues :=: flatten (improviseOverD7 <$> cBlues) ~> P8
-
-    improviseOverD7 :: Chord -> Melody
-    improviseOverD7 (a:b:c:d:_) =
-      a <| qn :+: (b <~ Mi2) <| en :+: b <| en :+:
-        (c <| qn :=: d <| qn) :+: (en~~) :+: d <| en
 
 example :: MusicGenerator () Melody
 example = do
