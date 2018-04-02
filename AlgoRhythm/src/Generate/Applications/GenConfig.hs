7b84cf63ebf97cdd669e220755463be980121c3e
Cas van der Rest <casmb11@xs4all.nl>
2018-04-03 00:31:03 +0200

    Missing config file

---
+++ AlgoRhythm/src/Generate/Applications/GenConfig.hs
@@ -0,0 +1,22 @@
+-- | Used by the Diatonic generator to steer the generation process
+module Generate.Applications.GenConfig (
+    GenConfig (..)
+  , Density (..)
+  , defaultGenConfig
+) where
+
+  import Music
+
+
+  -- | Denotes the global note density in a piece of music
+  data Density = High | Medium | Low
+
+  data GenConfig = GenConfig { key                :: PitchClass
+                             , baseScale          :: [Interval]
+                             , chords             :: Music SemiChord
+                             , phraseDistribution :: [(Int, Density)]
+                             , octaveDistribution :: [(Int, Octave)]
+                             }
+
+  defaultGenConfig :: GenConfig
+  defaultGenConfig = undefined
